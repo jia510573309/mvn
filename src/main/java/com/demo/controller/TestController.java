15a46d46a85362022f51ee20ea99cf2157d8f1de
Jia Min <510573309@qq.com>
2018-11-19 16:29:51 +0800

    添加自定义菜单功能但是个人公众号并不支持，所以然并卵

--- src/main/java/com/demo/controller/TestController.java
+++ src/main/java/com/demo/controller/TestController.java
@@ -4,26 +4,63 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.demo.utils.PinYinUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.demo.menu.ClickButton;
+import com.demo.menu.ViewButton;
+import com.demo.utils.HttpUtils;
 
+import net.sf.json.JSONArray;
 
 @Controller
 @RequestMapping("/")
 public class TestController {
 
 	@RequestMapping("index")
-	public String getIndex(){
+	public String getIndex() {
 		return "index.html";
 	}
-	
+
 	@RequestMapping("test")
-	public @ResponseBody String test(){
+	public @ResponseBody String test() {
 		return "mvn -- request Success ! ! !";
 	}
-	
+
 	public static void main(String[] args) {
-		String s = "加多宝";
-		String firstSpell = PinYinUtil.getFirstSpell(s);
-		System.out.println(firstSpell);
+		ClickButton cbt = new ClickButton();
+		cbt.setKey("image");
+		cbt.setName("回复图片");
+		cbt.setType("click");
+
+		ViewButton vbt = new ViewButton();
+		vbt.setUrl("http://www.baidu.com");
+		vbt.setName("百度");
+		vbt.setType("view");
+
+		JSONArray sub_button = new JSONArray();
+		sub_button.add(cbt);
+		sub_button.add(vbt);
+
+		JSONObject buttonOne = new JSONObject();
+		buttonOne.put("name", "菜单");
+		buttonOne.put("sub_button", sub_button);
+
+		JSONArray button = new JSONArray();
+		button.add(vbt);
+		button.add(buttonOne);
+		button.add(cbt);
+
+		JSONObject menujson = new JSONObject();
+		menujson.put("button", button);
+		System.out.println(menujson);
+		String url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token="
+				+ "15_U28ng0DN1vFiq2WjKfcp-HM2OWfRZizuxFam8ZQZRDc2fgFtTyl7apo6iFrFDKySQgaq0H3Pc1rqh1moljyI4bXb0LFy_FtkoVb0cXs_szg40w7O1SwW6TdAwlDGF2GL9muYrzjho9NOy1kNXVViABAMKK";
+
+		try {
+			String rs = HttpUtils.sendPost(url, menujson.toJSONString());
+			System.out.println(rs);
+		} catch (Exception e) {
+			System.out.println("请求错误！");
+		}
+
 	}
 }
