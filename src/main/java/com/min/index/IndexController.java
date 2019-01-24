50b94b7d9bbf086bb7a1e9536227e858c229370c
jia_min <510573309@qq.com>
2019-01-24 15:19:01 +0800

    跨域测试

--- src/main/java/com/min/index/IndexController.java
+++ src/main/java/com/min/index/IndexController.java
@@ -9,6 +9,6 @@ public class IndexController {
 	
 	@RequestMapping("index")
 	public String getIndex() {
-		return "微信公众号后台接口---最后更新于2019年1月24日14:04:23";
+		return "微信公众号后台接口---最后更新于2019年1月24日15:18:32";
 	}
 }
