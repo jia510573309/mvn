4a342cfeeee40190942f0b0251526ff684fc98b1
jia_min <510573309@qq.com>
2019-01-24 14:04:30 +0800

    测试更新

--- src/main/java/com/min/index/IndexController.java
+++ src/main/java/com/min/index/IndexController.java
@@ -9,6 +9,6 @@ public class IndexController {
 	
 	@RequestMapping("index")
 	public String getIndex() {
-		return "微信公众号后台接口--- update by 2019年1月11日17:07:34";
+		return "微信公众号后台接口---最后更新于2019年1月24日14:04:23";
 	}
 }
